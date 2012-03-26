a5cd6b44104b361054f11422d0a2c547d0f979d7
saasbook <saasbook@saasbook.(none)>
2012-03-25 22:27:12 -0700

    Got the HW3-2b part to work

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -15,6 +15,9 @@ module NavigationHelpers
 
     when /^the home\s?page$/
       '/'
+    
+    when /^the RottenPotatoes home\s?page$/
+      '/movies'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
